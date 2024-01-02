dcdfeb7a77a83ee36fb1a3d81f4049486c8accb0
Shurc1val <hughpm16@gmail.com>
2024-01-02 14:38:39 +0000

    created load.py file, and function definitions for functions used in pipeline.py

---
+++ pipeline/load.py
@@ -0,0 +1,16 @@
+"""Module to add the transformed data extracted from the kafka stream to the db."""
+
+
+def add_user(user: dict):
+    """adds user dictionary as a record in the User table in the db."""
+    pass
+
+
+def add_ride(ride: dict):
+    """adds ride dictionary as a record in the Ride table in the db."""
+    pass
+
+
+def add_reading(reading: dict):
+    """adds reading dictionary as a record in the Reading table in the db."""
+    pass
