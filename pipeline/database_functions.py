afde6c65305f4af59a9d5d705eaa61992a641278
CharlieDeanCS <deanc9116@gmail.com>
2024-01-02 16:40:28 +0000

    created the load user into database functionality

--- pipeline/database_functions.py
+++ pipeline/database_functions.py
@@ -18,3 +18,21 @@ def get_database_connection() -> extensions.connection:
                             database=environ["DATABASE_NAME"]
                             )
 
+
+def load_user_into_database(db_connection : extensions.connection, user : dict) -> int:
+    """Loads a user into the database using SQL."""
+
+    with db_connection.cursor() as db_cur:
+       
+            query = """INSERT INTO Rider(rider_id,address_id,first_name,last_name,birthdate,height,weight,
+            email,gender,account_created) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING rider_id;"""
+
+            parameters = (user['user_id'],user['address_id'],user['first_name'],
+                          user['last_name'],user['birthdate'],user['height'],
+                          user['weight'],user['email'],user['gender'],user['account_created'])
+
+            db_cur.execute(query,parameters)
+
+            rider_id = db_cur.fetchone()
+
+            return rider_id
--- pipeline/load.py
+++ pipeline/load.py
@@ -1,11 +1,13 @@
 """Module to add the transformed data extracted from the kafka stream to the db."""
-from database_functions import get_database_connection
+from database_functions import get_database_connection,load_user_into_database
 
-def add_user(user: dict):
+def add_user(user: dict) -> id:
     """adds user dictionary as a record in the Rider table in the db."""
     connection = get_database_connection()
 
-    
+    rider_id = load_user_into_database(connection,user)
+
+    return rider_id
 
 
 def add_ride(ride: dict) -> int:
