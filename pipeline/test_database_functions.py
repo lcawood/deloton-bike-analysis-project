31b6ed6ddae51843f2de057c036ab4f380b672ad
CharlieDeanCS <deanc9116@gmail.com>
2024-01-02 23:41:12 +0000

    created tests for loading rides into the database

--- pipeline/test_database_functions.py
+++ pipeline/test_database_functions.py
@@ -10,6 +10,8 @@ EXAMPLE_USER = {'user_id' : 1234,'address_id' : 6,'first_name': "Charlie",
 
 EXAMPLE_ADDRESS = {"first_line" : "63 Studio","second_line" : "Nursery Avenue", "city" : "London", "postcode" : "LA1 34A"}
 
+EXAMPLE_RIDE = {"user_id" : 1234, "bike_id": 1, "start_time" : "2021-07-03 16:21:12"}
+
 def test_load_address_into_database():
     """Tests that a address gets correctly loaded into the database"""
 
@@ -30,3 +32,88 @@ def test_load_address_into_database():
     mock_fetch.assert_called_once()
 
     assert address_id == 1
+
+def test_select_address_from_database():
+    """Tests that a address gets correctly selected from the database"""
+
+    mock_conn = MagicMock()
+
+    mock_execute = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .execute
+    mock_fetch = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .fetchone
+    
+    mock_fetch.return_value = (1,)
+
+    address_id = select_address_from_database(mock_conn, EXAMPLE_ADDRESS)
+
+    mock_execute.assert_called_once()
+    mock_fetch.assert_called_once()
+
+    assert address_id == 1
+
+
+def test_load_user_into_database():
+    """Tests that a user gets correctly loaded into the database"""
+
+    mock_conn = MagicMock()
+
+    mock_execute = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .execute
+    mock_fetch = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .fetchone
+    
+    mock_fetch.return_value = (EXAMPLE_USER["user_id"],)
+
+    user_id = load_user_into_database(mock_conn, EXAMPLE_USER)
+
+    mock_execute.assert_called_once()
+    mock_fetch.assert_called_once()
+
+    assert user_id == 1234
+
+def test_load_ride_into_database():
+    """Tests that a ride gets correctly loaded into the database"""
+
+    mock_conn = MagicMock()
+
+    mock_execute = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .execute
+    mock_fetch = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .fetchone
+    
+    mock_fetch.return_value = (1,)
+
+    ride_id = load_ride_into_database(mock_conn, EXAMPLE_RIDE)
+
+    mock_execute.assert_called_once()
+    mock_fetch.assert_called_once()
+
+    assert ride_id == 1
+
+def test_select_ride_from_database():
+    """Tests that a ride gets correctly selected from the database"""
+
+    mock_conn = MagicMock()
+
+    mock_execute = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .execute
+    mock_fetch = mock_conn.cursor.return_value\
+            .__enter__.return_value\
+            .fetchone
+    
+    mock_fetch.return_value = (1,)
+
+    ride_id = select_ride_from_database(mock_conn, EXAMPLE_RIDE)
+
+    mock_execute.assert_called_once()
+    mock_fetch.assert_called_once()
+
+    assert ride_id == 1
