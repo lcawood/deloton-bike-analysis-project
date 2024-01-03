3c51dc1d605ff31e5c14c847cc9e5adadd4729f7
CharlieDeanCS <deanc9116@gmail.com>
2024-01-03 16:26:40 +0000

    added lambda handler

--- daily_report/report.py
+++ daily_report/report.py
@@ -1,9 +1,34 @@
-"""Report code"""
+"""
+A script then when ran creates a html string a document that show
+a daily report of the rides from the previous day.
+The html body is returned in a lambda handler.
+The html document is sent to a S3 Bucket.
+"""
 
-def report():
-    """All code retaining to the report"""
-    return 0
+from os import environ
+import json
+
+from dotenv import load_dotenv
+
+def handler(event=None, context=None) -> int:
+    """Handler for the lambda function"""
+
+    try:
+
+        load_dotenv()
+
+        report_dict = create_report_data([])
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(report_dict["html_body"])
+        }
+    except Exception as e:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(e)
+        }
 
 
 if __name__ == "__main__":
-    report()
+    print(handler())
