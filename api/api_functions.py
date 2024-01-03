0f4f14d0881232e16559aef47dc35b8485ace930
Shurc1val <hughpm16@gmail.com>
2024-01-03 21:20:13 +0000

    added functionality to give reading information on rides

--- api/api_functions.py
+++ api/api_functions.py
@@ -7,7 +7,9 @@ functionality.
 from datetime import datetime
 from psycopg2.extensions import connection
 from psycopg2.errors import Error
+
 import database_functions
+from api_helper_functions import format_seconds_as_readable_time
 
 
 def get_ride(db_conn: connection, ride_id: int) -> (dict, int):
@@ -26,7 +28,7 @@ def get_ride(db_conn: connection, ride_id: int) -> (dict, int):
 
     if ride:
         return ride, 200
-    
+
     return {'error': f'Ride with id {ride_id} could not be found.'}, 404
 
 
@@ -46,19 +48,19 @@ def get_rider(db_conn: connection, rider_id: int) -> (dict, int):
 
     if rider:
         return rider, 200
-    
+
     return {'error': f'Rider with id {rider_id} could not be found.'}, 404
 
 
-def get_rider_rides(db_conn: connection, rider_id: int) -> (dict, int):
+def get_rider_rides(db_conn: connection, rider_id: int, expanded: bool, summary: bool) -> (dict, int):
     """
     Function to attempt to retrieve a list of all rides belonging to a rider with specified id from
     database using relevant database_functions, returning a list of dictionaries of said rider's
     rides with status code 200 if successful, and an error dict with appropriate status code if
     not.
     """
-    if not isinstance(rider_id, int):
-        return {'error': 'Invalid url; rider_id must be an integer.'}, 400
+    if (type(rider_id) != int) or (rider_id < 0):
+        return {'error': 'Invalid url; rider_id must be a positive integer.'}, 400
 
     try:
         rides = database_functions.get_rider_rides_by_id(db_conn, rider_id)
@@ -66,12 +68,20 @@ def get_rider_rides(db_conn: connection, rider_id: int) -> (dict, int):
         return {'error': str(e)}, 500
 
     if rides:
+        if expanded:
+            for i, ride in enumerate(rides):
+                rides[i]['readings'] = database_functions.get_readings_by_ride_id(db_conn, ride['ride_id'])
+        if summary:
+            for i, ride in enumerate(rides):
+                rides[i]['reading_summary'] = database_functions.get_readings_summary_for_ride_id(db_conn, ride['ride_id'])
+                rides[i]['reading_summary']['duration'] = format_seconds_as_readable_time(rides[i]['reading_summary']['duration'])
         return rides, 200
-    
+
     return {'error': f'Unable to locate any rides belonging to a rider with id {rider_id}.'}, 404
 
 
-def get_daily_rides(db_conn: connection, date: str = datetime.today().strftime("%d/%m/%Y")) -> (dict, int):
+def get_daily_rides(db_conn: connection,
+                    date: str = datetime.today().strftime("%d-%m-%Y")) -> (dict, int):
     """
     Function to attempt to retrieve a list of all rides starting on a specified date (defaulting
     to the current date) from database using relevant database_functions, returning a list of
@@ -79,9 +89,11 @@ def get_daily_rides(db_conn: connection, date: str = datetime.today().strftime("
     appropriate status code if not.
     """
     try:
-        date = datetime.strptime(date, "%d/%m/%Y").date()
-    except ValueError:
-        return {'error': 'Invalid url; date must be a datetime string matching the format dd/mm/yy.'}, 400
+        date = datetime.strptime(date, "%d-%m-%Y").date()
+    except (TypeError, ValueError):
+        return {
+            'error': 'Invalid url; date must be a datetime string matching the format dd-mm-yyyy.'
+            }, 400
 
     try:
         rides = database_functions.get_daily_rides(db_conn, date)
@@ -90,8 +102,8 @@ def get_daily_rides(db_conn: connection, date: str = datetime.today().strftime("
 
     if rides:
         return rides, 200
-    
-    return {'error': f'Unable to locate any rides starting on {date}.'}, 404
+
+    return {'error': f'Unable to locate any rides starting on {date.strftime("%d-%m-%Y")}.'}, 404
 
 
 def delete_ride(db_conn: connection, ride_id: int) -> (dict, int):
@@ -100,8 +112,8 @@ def delete_ride(db_conn: connection, ride_id: int) -> (dict, int):
     database_functions, returning a dictionary of said ride with status code 200 if successful,
     and an error dict with appropriate status code if not.
     """
-    if not isinstance(ride_id, int):
-        return {'error': 'Invalid url; ride_id must be an integer.'}, 400
+    if (type(ride_id) != int) or (ride_id < 0):
+        return {'error': 'Invalid url; ride_id must be a positive integer.'}, 400
 
     try:
         ride = database_functions.delete_ride_by_id(db_conn, ride_id)
@@ -110,5 +122,5 @@ def delete_ride(db_conn: connection, ride_id: int) -> (dict, int):
 
     if ride:
         return ride, 200
-    
+
     return {'error': f'Ride with id {ride_id} could not be found.'}, 404
