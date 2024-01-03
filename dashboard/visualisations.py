1bec99570c0924b56b353700a418c9883e94bfe1
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2024-01-03 14:42:17 +0000

    Added get rider name function base to visualisations.py

--- dashboard/visualisations.py
+++ dashboard/visualisations.py
@@ -4,3 +4,8 @@ Functions for visualising Deloton Bike ride data on the streamlit app.
 
 import pandas as pandas
 import altair as alt
+
+
+def get_current_ride_name():
+    """Returns a value containing the rider first and last name."""
+    pass
