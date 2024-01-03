5195d10bac92aeefbb39e13582f6f5cffe9c8f36
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2024-01-03 16:13:01 +0000

    Added functions to get highest metrics for current rider

--- dashboard/database.py
+++ dashboard/database.py
@@ -49,7 +49,7 @@ def get_current_ride_data(db_connection: extensions.connection) -> int:
         return user_details
 
 
-def get_current_rider_best_duration(db_cur: extensions.connection.cursor, rider_id: int):
+def get_current_rider_highest_duration(db_cur: extensions.connection.cursor, rider_id: int):
     """Returns the highest historical time_elapsed of the rider with the given rider_id."""
     query = """
     SELECT elapsed_time
@@ -64,14 +64,79 @@ def get_current_rider_best_duration(db_cur: extensions.connection.cursor, rider_
 
     db_cur.execute(query, (rider_id, ))
 
-    best_duration = db_cur.fetchone()
+    highest_duration = db_cur.fetchone()
 
-    return best_duration[0] if best_duration is not None else None
+    return highest_duration[0] if highest_duration is not None else None
 
 
-def get_current_ride_data_best(db_connection: extensions.connection, rider_details: list) -> int:
-    """Fetched the personal best details of the current ride from the database using an SQL Select Query."""
+def get_current_rider_highest_heart_rate(db_cur: extensions.connection.cursor, rider_id: int):
+    """Returns the highest historical heart_rate of the rider with the given rider_id."""
+    query = """
+    SELECT heart_rate
+    FROM ride
+    JOIN Rider ON Ride.rider_id = Rider.rider_id
+    JOIN Reading ON Ride.ride_id = Reading.ride_id
+    WHERE Ride.rider_id LIKE %s
+    ORDER BY elapsed_time DESC
+    LIMIT 1
+    ;
+    """
+
+    db_cur.execute(query, (rider_id, ))
+
+    highest_heart_rate = db_cur.fetchone()
+
+    return highest_heart_rate[0] if highest_heart_rate is not None else None
+
+
+def get_current_rider_highest_power(db_cur: extensions.connection.cursor, rider_id: int):
+    """Returns the highest historical power of the rider with the given rider_id."""
+    query = """
+    SELECT power
+    FROM ride
+    JOIN Rider ON Ride.rider_id = Rider.rider_id
+    JOIN Reading ON Ride.ride_id = Reading.ride_id
+    WHERE Ride.rider_id LIKE %s
+    ORDER BY elapsed_time DESC
+    LIMIT 1
+    ;
+    """
+
+    db_cur.execute(query, (rider_id, ))
+
+    highest_power = db_cur.fetchone()
+
+    return highest_power[0] if highest_power is not None else None
+
+
+def get_current_rider_highest_resistance(db_cur: extensions.connection.cursor, rider_id: int):
+    """Returns the highest historical resistance of the rider with the given rider_id."""
+    query = """
+    SELECT resistance
+    FROM ride
+    JOIN Rider ON Ride.rider_id = Rider.rider_id
+    JOIN Reading ON Ride.ride_id = Reading.ride_id
+    WHERE Ride.rider_id LIKE %s
+    ORDER BY elapsed_time DESC
+    LIMIT 1
+    ;
+    """
+
+    db_cur.execute(query, (rider_id, ))
+
+    highest_resistance = db_cur.fetchone()
+
+    return highest_resistance[0] if highest_resistance is not None else None
+
+
+def get_current_ride_data_highest(db_connection: extensions.connection, rider_details: list) -> int:
+    """Fetched the personal highest details of the current ride from the database using an SQL Select Query."""
 
     with db_connection.cursor() as db_cur:
         rider_id = rider_details[0]
-        best_duration = get_current_rider_best_duration(db_cur, rider_id)
+        highest_duration = get_current_rider_highest_duration(db_cur, rider_id)
+        highest_heart_rate = get_current_rider_highest_heart_rate(
+            db_cur, rider_id)
+        highest_power = get_current_rider_highest_power(db_cur, rider_id)
+        highest_resistance = get_current_rider_highest_resistance(
+            db_cur, rider_id)
