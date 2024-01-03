cbbae0f89f816e7223f4a5a40cf81567de732e4a
Shurc1val <hughpm16@gmail.com>
2024-01-03 21:21:01 +0000

    made a function to convert time between seconds and human readable string

---
+++ api/api_helper_functions.py
@@ -0,0 +1,22 @@
+
+from datetime import timedelta
+
+def format_seconds_as_readable_time(seconds: int) -> str:
+    """
+    Takes in an integer number of seconds, and returns a human readable string of the time in
+    hours, minutes and seconds.
+    """
+    hours = seconds // (60 ** 2)
+    seconds -= hours * (60 ** 2)
+    minutes = seconds // 60
+    seconds -= minutes * 60
+
+    time_string = f"{seconds}s"
+
+    if minutes:
+        time_string = f'{minutes}m ' + time_string
+
+    if hours:
+        time_string = f'{hours}h ' + time_string
+        
+    return time_string
