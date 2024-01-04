c37640690a4d7d99254cbeb50882fd9f59e441df
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2024-01-04 11:24:45 +0000

    Refactored code into main current ride function

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,6 +9,7 @@ from os import environ
 
 from dotenv import load_dotenv
 from pandas import DataFrame
+from psycopg2 import extensions
 import streamlit as st
 
 from database import (get_database_connection,
@@ -16,6 +17,29 @@ from database import (get_database_connection,
 from utilities import get_current_rider_name
 from visualisations import (
     get_current_ride_header, get_dashboard_title, get_current_ride_header_personal_info, get_current_ride_metrics, get_current_ride_personal_best_metrics)
+
+
+def main_current_ride(db_connection: extensions.connection) -> None:
+    """
+    Main function that calls all the functions related to
+    displaying the current ride visualisations.
+    """
+
+    current_ride = get_current_ride_data(conn)
+    current_ride_personal_best = get_current_ride_data_highest(
+        conn, current_ride)
+
+    rider_name = get_current_rider_name(current_ride)
+
+    get_dashboard_title()
+
+    get_current_ride_header(rider_name)
+
+    get_current_ride_header_personal_info(current_ride)
+
+    get_current_ride_metrics(current_ride)
+
+    get_current_ride_personal_best_metrics(current_ride_personal_best)
 
 
 if __name__ == "__main__":
