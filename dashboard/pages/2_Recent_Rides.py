fedf168f7e86cd1178348bd5d1285046cbd7835d
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2024-01-04 21:27:50 +0000

    Added recent rides page

--- dashboard/pages/2_Recent_Rides.py
+++ dashboard/pages/2_Recent_Rides.py
@@ -1,5 +1,5 @@
 """
-Dashboard script for the current ride to establish connection to the RDS database, fetch data using SQL queries and
+Dashboard script for recent rides to establish connection to the RDS database, fetch data using SQL queries and
 create visualisations in a Streamlit app (using functions from the `database.py`,
 `visualisations.py` and `utilities.pyz files as necessary).
 """
@@ -23,39 +23,6 @@ from visualisations import (get_current_ride_header, get_dashboard_title,
 CURRENT_RIDE_REFRESH_RATE = 5
 RECENT_RIDE_REFRESH_RATE = 5
 LAST_UPDATED_COUNT_INCREMENT = 1
-
-
-def main_current_ride(db_connection: extensions.connection) -> None:
-    """
-    Main function that calls all the functions related to
-    displaying the current ride visualisations.
-    """
-    with st.container():
-        current_ride = get_current_ride_data(db_connection)
-        current_ride_personal_best = get_current_ride_data_highest(
-            db_connection, current_ride)
-
-        rider_name = get_current_rider_name(current_ride)
-
-        get_current_ride_header(rider_name)
-
-        # Placeholder for last updated time caption
-        empty_last_updated_placeholder = st.empty()
-
-        get_current_ride_header_personal_info(current_ride)
-
-        get_current_ride_metrics(current_ride)
-
-        # readings to monitor for heart rate warning
-        heart_rate = current_ride[7]
-        elapsed_time = current_ride[10]
-
-        if is_heart_rate_abnormal(current_ride) and elapsed_time > 10:
-            get_heart_rate_warning(heart_rate)
-
-        get_current_ride_personal_best_metrics(current_ride_personal_best)
-
-        return empty_last_updated_placeholder
 
 
 def main_recent_rides(db_connection: extensions.connection) -> None:
@@ -82,16 +49,11 @@ if __name__ == "__main__":
 
     get_dashboard_title()
     while True:
-        # Auto-refresh the current ride section
-        last_updated_placeholder_current = main_current_ride(conn)
-        update_time = datetime.now()
-
+        # Auto-refresh the page
         last_updated_placeholder_recent = main_recent_rides(conn)
         update_time = datetime.now()
 
         for i in range(CURRENT_RIDE_REFRESH_RATE):
-            get_last_updated_current_ride(
-                update_time, last_updated_placeholder_current)
             get_last_updated_current_ride(
                 update_time, last_updated_placeholder_recent)
             time.sleep(LAST_UPDATED_COUNT_INCREMENT)
