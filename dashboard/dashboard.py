cab7c206c501064ae7e7722f791eb79f448fbfbd
dawiddawidowski <trainee.dawid.dawidowski@hotmail.co.uk>
2024-01-08 16:45:09 +0000

    Added test file and modified homepage dashboard.py file

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,28 +7,36 @@ Entry point home page for the dashboard.
 
 import streamlit as st
 
-st.set_page_config(
-    page_title="DELOTON Dashboard",
-    page_icon="🚲",
-    layout="wide"
-)
 
-st.write("# DELOTON Bike Analysis🚴")
+def main_homepage():
+    """Generates the visualisations for the dashboard home page."""
+    st.set_page_config(
+        page_title="DELOTON Dashboard",
+        page_icon="🚲",
+        layout="wide"
+    )
 
-st.markdown('''
-<style>
-.st-b7 {
-    color: #90d1a2;
-}
-</style>
-''', unsafe_allow_html=True)
+    st.write("# DELOTON Bike Analysis🚴")
 
-st.sidebar.success("Select a page above.")
+    st.markdown('''
+    <style>
+    .st-b7 {
+        color: #90d1a2;
+    }
+    </style>
+    ''', unsafe_allow_html=True)
 
-st.markdown(
-    """
-    Realtime dashboard to give the business visibility on the
-    current and recent behaviour of riders.\n
+    st.sidebar.success("Select a page above.")
 
-    **👈 Select a page from the sidebar** to see the current or recent rides.
-    """)
+    st.markdown(
+        """
+        Realtime dashboard to give the business visibility on the
+        current and recent behaviour of riders.\n
+
+        **👈 Select a page from the sidebar** to see the current or recent rides.
+        """)
+
+
+if __name__ == "__main__":
+
+    main_homepage()
---
+++ dashboard/test_dashboard.py
@@ -0,0 +1,8 @@
+"""Unit tests for the dashboard.py file"""
+
+from dashboard import main_homepage
+
+
+def test_dashboard():
+    """Tests the dashboard code"""
+    assert True
